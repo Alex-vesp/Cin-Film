1860503a1194c8927f55bac2ca7670b3217d8554
alexp <alex.poncy@etu.univ-amu.fr>
2021-05-06 15:29:45 +0200

    formulaire connection en pop up et inscription

--- Cine-Film/src/Javascript/js.js
+++ Cine-Film/src/Javascript/js.js
@@ -1,15 +1,30 @@
-/*window.onclick = function (event) {
-    if (event.target != document.getElementById("form") && event.target !=  document.getElementById("connection") ){
-        console.log(document.getElementById("form"))
-        document.getElementById("myForm").style.display = "none";
+window.onclick = function (event) {
+    if (event.target != document.getElementById("formConnec") && event.target !=  document.getElementById("connection") &&  event.target !=  document.getElementById("btnSeConnecter") && event.target != document.getElementById("usernameConnec") && event.target != document.getElementById("pswConnec") && event.target != document.getElementById("pswForgotten") && event.target != document.getElementById("seConnecter")){
+        console.log(document.getElementById("formConnec"));
+        console.log(event.target);
+        document.getElementById("myFormConnec").style.display = "none";
     }
 
-}; */
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
+    if (event.target != document.getElementById("formInscr") && event.target !=  document.getElementById("inscription") &&  event.target !=  document.getElementById("btnInscription") && event.target != document.getElementById("usernameInscr") && event.target != document.getElementById("emailInscr") && event.target != document.getElementById("pswInscr") && event.target != document.getElementById("s'inscrire")){
+        console.log(document.getElementById("formInscr"));
+        console.log(event.target);
+        document.getElementById("myFormInscr").style.display = "none";
+    }
+
+
+};
+function openFormConnec() {
+    document.getElementById("myFormConnec").style.display = "block";
 }
 
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+function closeFormConnec() {
+    document.getElementById("myFormConnec").style.display = "none";
 }
 
+function openFormInscr() {
+    document.getElementById("myFormInscr").style.display = "block";
+}
+
+function closeFormInscr() {
+    document.getElementById("myFormInscr").style.display = "none";
+}
