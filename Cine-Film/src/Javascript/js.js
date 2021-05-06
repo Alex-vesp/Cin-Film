edd30a3caabc67844b66589abc75322d35c0ea4b
alexp <alex.poncy@etu.univ-amu.fr>
2021-05-06 02:04:18 +0200

    début js, formulaire connection en pop up

---
+++ Cine-Film/src/Javascript/js.js
@@ -0,0 +1,15 @@
+/*window.onclick = function (event) {
+    if (event.target != document.getElementById("form") && event.target !=  document.getElementById("connection") ){
+        console.log(document.getElementById("form"))
+        document.getElementById("myForm").style.display = "none";
+    }
+
+}; */
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
