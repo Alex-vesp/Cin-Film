c53071faf839a8e15d890aca378d7043bf8b1d1c
Raph <raphael.bz@hotmail.fr>
2021-05-17 23:30:46 +0200

    methode pour film populaire on point
    ouais le JSON ouais créé

--- Cine-Film/src/public/javascript/apiRequests.js
+++ Cine-Film/src/public/javascript/apiRequests.js
@@ -17,19 +17,32 @@ const {response} = require("express");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const APIKEY = '45c7524a7adb09538e00b07bff05d4e3';
+const APIKEY = '?api_key=45c7524a7adb09538e00b07bff05d4e3';
 let baseURL = 'https://api.themoviedb.org/3/';
-let searchURL = 'search/movie?api_key=';
+let searchURL = 'search/movie';
+let discoverMovie = 'discover/movie?api_key=';
+let release2010 = '&primary_release_year=2010&sort_by=vote_average.desc';
 let baseSearchURL = baseURL + searchURL + APIKEY + '&query=';
 let changeLanguageToFr = '&language=fr';
-let newText;
+let popularMovies = 'movie/popular';
+let baseRatedURL = baseURL + discoverMovie + APIKEY + changeLanguageToFr + release2010;
+
+
+let popularSearchURL = baseURL + popularMovies + APIKEY + changeLanguageToFr;
+
+let popularSearch = function (){
+    for(let i = 0; i < 20; i++) {
+        fetch(popularSearchURL).then(response => response.text()).then(data => console.log(JSON.parse(data).results[i].title)).catch(reason => reason);
+    }
+}
 
 let movieSearch = function (keyword){
     let concatenatedKeyword = keyword.replace(/ /g, '%20')
     let url = ''.concat(baseSearchURL, concatenatedKeyword, changeLanguageToFr);
-    fetch(url).then(response => response.text()).then(text => newText = JSON.parse(text).results[0]);
-
+        fetch(url).then(response => response.text()).then(data =>
+            console.log(JSON.parse(data).results[0]))
 }
 
+//movieSearch("les dents de la mer");
+popularSearch();
 
-movieSearch("les dents de la mer");
