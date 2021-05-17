fc77ed4a8d3ba03c4c3b840164633247abc49635
alexp <poncyalex@yahoo.fr>
2021-05-17 23:47:42 +0200

    fin fonctions connexion et inscription

--- Cine-Film/src/server.js
+++ Cine-Film/src/server.js
@@ -3,13 +3,9 @@
 var express = require('express');
 var mustache = require('mustache-express');
 
+
 var model = require('./model');
 var app = express();
-var isAuthentificated = false;
-
-
-
-
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -40,7 +36,6 @@ function is_authenticated(req, res, next) {
 // ajoute deux variables de session aux templates : authenticated et le nom de l'utilisateur
 app.use(function(req, res, next) {
     if(req.session.user !== undefined) {
-        isAuthentificated = true;
         res.locals.authenticated = true;
         res.locals.name = req.session.name;
     }
@@ -52,30 +47,32 @@ app.use(function(req, res, next) {
 
 
 
-app.post('pageConnexion.html', (req, res) => {
-    res.redirect("index.html");
-    const user = model.login(req.body.pseudoConnec, req.body.mdpConnec);
+app.post('/login', (req, res) => {
+    console.log("loginnn");
+    const user = model.login(req.body.user, req.body.password);
     if(user != -1) {
         req.session.user = user;
-        req.session.name = req.body.pseudoConnec;
-        window.alert(req.session.name);
+        req.session.name = req.body.user;
         res.redirect('index.html');
-        window.alert(req.session.name);
     } else {
         res.redirect('pageConnexion.html');
     }
 });
 
 app.post('/pageInscription.html', (req, res) => {
-    const user = model.new_user(req.body.pseudo, req.body.mail, req.body.mdp, req.body.nom, req.body.prenom, req.body.dateN, req.body.genre, req.body.acteur, req.body.realisateur );
-    if(user != -1) {
-        req.session.user = user;
-        req.session.name = req.body.pseudoConnec;
-        window.alert(req.session.name);
-        res.redirect('index.html');
-        window.alert(req.session.name);
-    } else {
-        res.redirect('/pageInscription.html');
+    if (req.body.mdp === req.body.mdpconfirm) {
+        const idActeur = model.searchActeur(req.body.prefAct);
+        const idReal = model.searchRealisateur(req.body.prefReal);
+        console.log(req.body.mdp);
+        const user = model.new_user(req.body.pseudo, req.body.mail, req.body.mdp, req.body.nom, req.body.prenom, req.body.dateN, req.body.prefGenre, idActeur, idReal );
+        if(user != -1) {
+            req.session.user = user;
+            req.session.name = req.body.user;
+            const connected = model.new_user(req.body.pseudo, req.body.mdp);
+            res.redirect('index.html');
+        } else {
+            res.redirect('/pageInscription.html');
+        }
     }
 });
 
@@ -91,13 +88,23 @@ app.get('/index.html', (req, res) => {
     res.render('index', found);
 });
 
-app.get('/pageConnecte.html', (req, res) => {
+/*app.get('/pageConnecte.html', (req, res) => {
     res.render('pageConnecte');
-});
+});*/
 
 app.get('/pageConnexion.html', (req, res) => {
     res.render('pageConnexion');
 });
+
+app.get('/login', (req, res) => {
+    console.log("loginnn2");
+    console.log(req.body.user);
+    var yes = model.login(req.params.user, req.body.password);
+    res.render('pageConnexion');
+
+
+});
+
 
 
 app.get('/pageFilm.html/:id', (req, res) => {
